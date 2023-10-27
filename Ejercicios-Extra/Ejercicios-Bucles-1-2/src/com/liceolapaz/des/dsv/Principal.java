4375289984037b8cd4b2279032b9373c68f76562
imdaano <danisanchezpsc@gmail.com>
2023-10-27 09:40:29 +0200

    new exercise 2

---
+++ Ejercicios-Extra/Ejercicios-Bucles-1-2/src/com/liceolapaz/des/dsv/Principal.java
@@ -0,0 +1,13 @@
+package com.liceolapaz.des.dsv;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        //  Crea un programa que imprima los números pares hasta 20 (inclusive).
+        int i = 0;
+        // Crear bucle
+        for (i = 0; i<=20; i+=2) {
+            System.out.println(i);
+        }
+    }
+}
